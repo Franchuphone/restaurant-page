1a130028898eb9e4f463fbf2d7b324ec7ef753eb
Franchuphone <222453306+Franchuphone@users.noreply.github.com>
2026-02-13 13:06:03 +0100

    Add CSS animations on content display Add JS module structure Add homepage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
-export { getContent } from './homepage';
+import { content, getHomeContent } from './homepage';
+import { getMenuContent } from './menu';
+import { getAboutContent } from './about';
+import { getLocationContent } from './location';
 import './style.css'
 
-// export getContent() from "./homepage.js";
+function cleanContent() {
+    const body = document.querySelector( "body" );
+    content.textContent = "";
+    content.remove();
+    body.append( content );
+}
+
+document.querySelector( ".nav-home" ).addEventListener( "click", () => {
+    cleanContent();
+    getHomeContent();
+} );
+
+document.querySelector( ".nav-menu" ).addEventListener( "click", () => {
+    cleanContent();
+    getMenuContent();
+} );
+
+document.querySelector( ".nav-location" ).addEventListener( "click", () => {
+    cleanContent();
+    getLocationContent();
+} );
+
+document.querySelector( ".nav-about" ).addEventListener( "click", () => {
+    cleanContent();
+    getAboutContent();
+} );
